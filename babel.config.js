e06a84f3e36304e418c7a2158436a1aa28ad6e34
lika85456 <lika85456@post.cz>
2022-06-09 21:52:59 +0200

    feat: jest testing

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+/**
+ * @see https://stackoverflow.com/questions/58613492/how-to-resolve-cannot-use-import-statement-outside-a-module-in-jest
+ */
+module.exports = {presets: ['@babel/preset-env']}
---
+++ tests/test.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+describe('Jest test', () => {
+    it('tests', () => {
+        expect(true);
+    });
+
+    it('tests react components', async () => {
+        render(<div>NEVER GONNA GIVE YOU UP!</div>);
+        // expect screen to contain text
+        expect(screen.getByText('NEVER GONNA GIVE YOU UP!')).toBeInTheDocument();
+    });
+});
